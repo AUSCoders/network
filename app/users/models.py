ed5d2f03079c196a8c1ce158dc6215f030659e30
AsadbekNurmamatov2002 <nurmamatovasadbek85@gmail.com>
2024-05-10 11:22:24 +0500

    m5

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import string
+import random
 
+from typing import Iterable
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 # Create your models here.
+
+
+def rand_slug():
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+
+class Profile(models.Model):
+    last_name=models.CharField(max_length=150, blank=True)
+    first_name=models.CharField(max_length=150, blank=True)
+    bio=models.TextField(max_length=600, default="O\'zinggiz haqida ma\'lomot....")
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    frinds=models.ManyToManyField(User, blank=True)
+    email=models.EmailField(unique=True, blank=True)
+    avater=models.ImageField(default="avatar.png", upload_to="avatars/")
+    country=models.CharField(max_length=120, default="Hi")
+    slug=models.SlugField(blank=True)
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.last_name}-{self.first_name}-{self.created}"
+    
+    def save(self, *args, **kwargs) -> None:
+        ew=None
+        if self.first_name and self.first_name :
+            to_slug=slugify(str(self.last_name).lower()+""+str(self.first_name))
+            ew=Profile.objects.filter(slug=to_slug).exists()
+            while ew:
+                to_slug=slugify(str(rand_slug)+""+str(self.first_name).capitalize()[::-1]+str(self.last_name)[::-1])
+                ew=Profile.objects.filter(slug=to_slug).exists()
+            return to_slug
+        return super().save(*args, **kwargs)
+    
+    
+    
+    
