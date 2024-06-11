8410363677825a060a635ce0511707e4075726c5
AsadbekNurmamtov2002 <nurmamatovasadbek85@gmail.com>
2024-06-11 15:54:08 +0500

    railwat.app

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,8 +14,13 @@ SECRET_KEY = 'django-insecure-fw1p6d^)puf%1*l6$ybe#cp#!=qijwov7q3ov^jla2j+)gr8z1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
 
+if DEBUG:
+    ALLOWED_HOSTS+=[
+        "127.0.0.1",
+        "localhost",
+    ]
 
 # Application definition
 
